7d66532cb811f1a98577e7fc464034739c7eb96b
= <57069030+CuongNguyenDang@users.noreply.github.com>
2019-12-12 06:03:32 +0700

    Done

---
+++ Assignment/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            lblMax = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            lblMin = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            lblAvg = new System.Windows.Forms.Label();
+            this.lblReport = new System.Windows.Forms.Label();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.lblList = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(16, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhập số nguyên :";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(393, 30);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 31);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemove.Location = new System.Drawing.Point(478, 30);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 31);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Xóa";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(16, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Danh sách:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(16, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 22);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Giá trị lớn nhất :";
+            // 
+            // lblMax
+            // 
+            lblMax.AutoSize = true;
+            lblMax.BackColor = System.Drawing.Color.Transparent;
+            lblMax.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblMax.ForeColor = System.Drawing.Color.White;
+            lblMax.Location = new System.Drawing.Point(179, 141);
+            lblMax.Name = "lblMax";
+            lblMax.Size = new System.Drawing.Size(0, 22);
+            lblMax.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(16, 212);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 22);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Giá trị nhỏ nhất :";
+            // 
+            // lblMin
+            // 
+            lblMin.AutoSize = true;
+            lblMin.BackColor = System.Drawing.Color.Transparent;
+            lblMin.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblMin.ForeColor = System.Drawing.Color.White;
+            lblMin.Location = new System.Drawing.Point(179, 212);
+            lblMin.Name = "lblMin";
+            lblMin.Size = new System.Drawing.Size(0, 22);
+            lblMin.TabIndex = 3;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(16, 281);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(156, 22);
+            this.label7.TabIndex = 3;
+            this.label7.Text = "Giá trị trung bình :";
+            // 
+            // lblAvg
+            // 
+            lblAvg.AutoSize = true;
+            lblAvg.BackColor = System.Drawing.Color.Transparent;
+            lblAvg.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            lblAvg.ForeColor = System.Drawing.Color.White;
+            lblAvg.Location = new System.Drawing.Point(179, 281);
+            lblAvg.Name = "lblAvg";
+            lblAvg.Size = new System.Drawing.Size(0, 22);
+            lblAvg.TabIndex = 3;
+            // 
+            // lblReport
+            // 
+            this.lblReport.AutoSize = true;
+            this.lblReport.BackColor = System.Drawing.Color.Transparent;
+            this.lblReport.Font = new System.Drawing.Font("Times New Roman", 7.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblReport.ForeColor = System.Drawing.Color.White;
+            this.lblReport.Location = new System.Drawing.Point(497, 306);
+            this.lblReport.Name = "lblReport";
+            this.lblReport.Size = new System.Drawing.Size(0, 15);
+            this.lblReport.TabIndex = 3;
+            // 
+            // txtInput
+            // 
+            this.txtInput.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(183, 30);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(194, 30);
+            this.txtInput.TabIndex = 4;
+            this.txtInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtInput_KeyDown);
+            // 
+            // lblList
+            // 
+            this.lblList.AutoSize = true;
+            this.lblList.BackColor = System.Drawing.Color.Transparent;
+            this.lblList.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblList.ForeColor = System.Drawing.Color.White;
+            this.lblList.Location = new System.Drawing.Point(179, 89);
+            this.lblList.Name = "lblList";
+            this.lblList.Size = new System.Drawing.Size(0, 22);
+            this.lblList.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Assignment.Properties.Resources.background;
+            this.ClientSize = new System.Drawing.Size(566, 321);
+            this.Controls.Add(this.lblList);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(lblAvg);
+            this.Controls.Add(lblMin);
+            this.Controls.Add(lblMax);
+            this.Controls.Add(this.lblReport);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Demo Observer Pattern";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblReport;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Label lblList;
+        internal static System.Windows.Forms.Label lblMax;
+        internal static System.Windows.Forms.Label lblMin;
+        internal static System.Windows.Forms.Label lblAvg;
+    }
+}
+
---
+++ Assignment/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Demo;
+namespace Assignment
+{
+    public partial class Form1 : Form
+    {
+        static IObservable ob = new Observable();
+        static void Register()
+        {
+            IObserver max = new Demo.Max(ob);
+            IObserver min = new Demo.Min(ob);
+            IObserver mean = new Demo.Mean(ob);
+            ob.attach(max);
+            ob.attach(min);
+            ob.attach(mean);
+        }
+        List<int> L = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+            lblList.Text = lblMax.Text = lblAvg.Text = lblMin.Text = lblReport.Text = "";
+            MinimizeBox = false;
+            MaximizeBox = false;
+            Register();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int temp = 0;
+            bool report = true;
+            try
+            {
+                temp = Int32.Parse(txtInput.Text);
+            }
+            catch (Exception)
+            {
+                report = false;
+            }
+            if (report)
+            {
+                lblReport.Text = "Thành công";
+                L.Add(temp);
+                string aux = "";
+                foreach (int i in L)
+                {
+                    aux = aux + i.ToString() + ' ';
+                }
+                lblList.Text = aux;
+                ob.setData(L);
+                ob.notify();
+            }
+            else
+                lblReport.Text = "Thất bại";
+            txtInput.Text = "";
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            int temp = 0;
+            bool report = true;
+            if (L.Count == 0) report = false;
+            try
+            {
+                temp = Int32.Parse(txtInput.Text);
+            }
+            catch (Exception)
+            {
+                report = false;
+            }
+            if (report)
+            {
+                bool isOk = L.Remove(temp);
+                if (!isOk) lblReport.Text = "Thất bại";
+                else lblReport.Text = "Thành công";
+                string aux = "";
+                foreach (int i in L)
+                {
+                    aux = aux + i.ToString() + ' ';
+                }
+                lblList.Text = aux;
+                lblList.Text = aux;
+                ob.setData(L);
+                ob.notify();
+            }
+            else
+                lblReport.Text = "Thất bại";
+            txtInput.Text = "";
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btnAdd_Click(sender, e);
+        }
+
+
+    }
+}
